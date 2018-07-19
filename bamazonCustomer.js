2264ca60f49e491a176b762d793a00372b2f3cc0
Paul <hoffsbox@gmail.com>
2018-07-19 12:34:22 -0400

    Lunch Break

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,9 @@
 // create global variables
+var fs = require("fs");
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-// create the connection information for the sql database
+// create the connection information for the sql database.
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,7 +13,7 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
   });
 
-// Establish connection to mySQL server & database
+// Establish connection to mySQL server & database.
 
 connection.connect(function(err){
     if (err) throw err;
@@ -20,21 +21,55 @@ connection.connect(function(err){
     afterConnection()
 });
 
-// Print contents of bamazon database
+// Print contents of bamazon database.
 
 function afterConnection() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.log("=================== Our Products ===================");
-        //console.log(/b)
+        console.log("");
+        console.log("============================== Our Products ===============================");
+        console.log("");
 
         for (i=0; i < res.length; i++){
-            console.log(res[i].item_id + ". "  + res[i].product_name + " " + "Department: " + res[i].department_name + " " + "$" + res[i].price + " " + "Current Stock: " + res[i].stock_quantity);
-//            connection.end();
-        }
-        
-    })
+            console.log(res[i].item_id + ". "  + res[i].product_name + ", " + "Department: " + res[i].department_name + ", " + "Price: " + "$" + res[i].price + ", " + "Current Stock: " + res[i].stock_quantity);
+        } 
 
+        console.log("");
+        console.log("=============================================================================");
+        console.log("");
+
+       order()
+    })
+}
+
+// Use Inquirer to query customer as to what product they would like to order & what quantity.
+
+function order() {
+    // query the database for all items in stock
+    connection.query("SELECT * FROM bamazon_db", function(err, results) {
+    if (err) throw err;
+
+    inquirer
+        .prompt([
+            {
+                name: "choice",
+                type: "rawlist",
+                choices: function() {
+                    var choiceArray = [];
+                    for (var i = 0; i , results.length; i++) {
+                        choiceArray.push(results[i].product_name);
+                    }
+                    return choiceArray;
+                },
+                message: "What item would you like to purchase?"
+            },
+            {
+                name: "purchase",
+                type: "input",
+                message: "How many would you like to purchase?"
+            }
+        ])
+    });
 }
 
 
